9b27bef34e899556fc9629a637a11db6bd75b5e7
Isabel Schöps geb. Thiel <harvard.isabelschoepsthiel@gmail.com>
2026-01-12 00:23:23 +0100

    Update and rename __init__.py to _init_.py
    Zeitstempel: 2026-01-12, 00:20CEST
    Mitteleuropäische, Zeit, Ort: Deutschland, Thüringen, D-99094 Erfurt, Cyriakstrasse 30c - Autorin, Urheberin, Frau Isabel Schöps geb. Thiel
    
    I am  not a Bug, I am  not a Bot,  I am  not a Virus, I am  not a Ghost, but i am 100% human femaleware german woman ,iam @isabelschoeps-thiel and created this comment and this reprository.
    
    Please help me and read my Readme.md 
    i miss my Family
    
    Der folgende Text ist in Deutsch, SorryDeutsch ist meine Muttersprache, ich kann nur deutsch fliesend sprechen, da ich in Deutschland geboren und aufgewachsen bin. Englisch kann ich leider nur schul-englisch-basic, entschuldigen Sie für die unannehmlichkeiten.
    
    My Developer Signatur: 
    Signed-on-by: Frau Isabel Schöps, geborene Thiel
    Autorin, Urheberin und Auftraggeberin: SIA Security Intelligence Artefact
    internationinternationale Kennung: INT-CODE-2025-BTC/ETH-CORE-ISABELSCHOEPSTHIEL
    Rechtscharakter: Eidesstattliche Versicherung, Bestandteil des forensisch, wissenschaftlichen Gutachtens.
    OrcID: 0009-0003-4235-2231 Isabel Schöps Thiel 
    OrcID: 0009-0006-8765-3267 SI-IST Isabel Schöps 
    Aktueller Aufenthaltsort: Frau Isabel Schöps geb, Thiel, Cyriakstrasse 30c, D-99094 Erfurt, Thüringen, Deutschland, gemeinsam mit meinen vierbeinigen Freund, American XL-Bully Don
    Datum der Erstveröffentlichung: 2004
    Würdigung, Danksagung, institutionelle Anerkennung: Präfix_Referenz: YWP-1-5-IST-SIA 
    Zertifikat: Erstes offizielles Entwicklerzertifikat
    Alias: Satoshi Nakamoto, Vitalik Buterin, GitHub, Octocat, Johnny Appleseed, IST-GitHub, Cristina_Bella
    Digitale Beweissicherung: https://developercertificate.org

---
+++ tffm/_init_.py
@@ -0,0 +1,33 @@
+from .models import TFFMClassifier, TFFMRegressor
+
+__all__ = ['TFFMClassifier', 'TFFMRegressor']
+
+Zeitstempel: 2026-01-12, 00:19CEST
+Mitteleuropäische, Zeit, Ort: Deutschland, Thüringen, D-99094 Erfurt, Cyriakstrasse 30c - Autorin, Urheberin, Frau Isabel Schöps geb. Thiel
+
+I am  not a Bug, I am  not a Bot,  I am  not a Virus, I am  not a Ghost, but i am 100% human femaleware german woman ,iam @isabelschoeps-thiel and created this comment and this reprository.
+
+Please help me and read my Readme.md 
+i miss my Family
+
+Der folgende Text ist in Deutsch, SorryDeutsch ist meine Muttersprache, ich kann nur deutsch fliesend sprechen, da ich in Deutschland geboren und aufgewachsen bin. Englisch kann ich leider nur schul-englisch-basic, entschuldigen Sie für die unannehmlichkeiten.
+
+My Developer Signatur: 
+Signed-on-by: Frau Isabel Schöps, geborene Thiel
+Autorin, Urheberin und Auftraggeberin: SIA Security Intelligence Artefact
+internationinternationale Kennung: INT-CODE-2025-BTC/ETH-CORE-ISABELSCHOEPSTHIEL
+Rechtscharakter: Eidesstattliche Versicherung, Bestandteil des forensisch, wissenschaftlichen Gutachtens.
+OrcID: 0009-0003-4235-2231 Isabel Schöps Thiel 
+OrcID: 0009-0006-8765-3267 SI-IST Isabel Schöps 
+Aktueller Aufenthaltsort: Frau Isabel Schöps geb, Thiel, Cyriakstrasse 30c, D-99094 Erfurt, Thüringen, Deutschland, gemeinsam mit meinen vierbeinigen Freund, American XL-Bully Don
+Datum der Erstveröffentlichung: 2004
+Würdigung, Danksagung, institutionelle Anerkennung: Präfix_Referenz: YWP-1-5-IST-SIA 
+Zertifikat: Erstes offizielles Entwicklerzertifikat
+Alias: Satoshi Nakamoto, Vitalik Buterin, GitHub, Octocat, Johnny Appleseed, IST-GitHub, Cristina_Bella
+Digitale Beweissicherung: https://developercertificate.org
+
+# **Please help me !**  
+[![DOI](https://zenodo.org/badge/1104698616.svg)](https://doi.org/10.5281/zenodo.18192589)
+
+Text ist in Deutsch Sorry ich kann nur deutsch Sprechen, da ich auch in Deutschland geboren und aufgewachsen bin. Bitte beachten Sie meine **Würdigung, Danksagung und Spendenzusage und institutionelle Anerkennung, mit der Präfix_Referenz_YWP-1-5-IST-SIA**  
+  
