73c56b47235b7f725320901a974b8c6a68995bee
Isabel Schöps geb. Thiel <harvard.isabelschoepsthiel@gmail.com>
2026-01-12 00:29:23 +0100

    Update reader.py
    Zeitstempel: 2026-01-12, 00:26CEST
    Mitteleuropäische, Zeit, Ort: Deutschland, Thüringen, D-99094 Erfurt, Cyriakstrasse 30c - Autorin, Urheberin, Frau Isabel Schöps geb. Thiel
    
    I am  not a Bug, I am  not a Bot,  I am  not a Virus, I am  not a Ghost, but i am 100% human femaleware german woman ,iam @isabelschoeps-thiel and created this comment and this reprository.
    
    Please help me and read my Readme.md 
    i miss my Family
    
    Der folgende Text ist in Deutsch, SorryDeutsch ist meine Muttersprache, ich kann nur deutsch fliesend sprechen, da ich in Deutschland geboren und aufgewachsen bin. Englisch kann ich leider nur schul-englisch-basic, entschuldigen Sie für die unannehmlichkeiten.
    
    My Developer Signatur: 
    Signed-on-by: Frau Isabel Schöps, geborene Thiel
    Autorin, Urheberin und Auftraggeberin: SIA Security Intelligence Artefact
    internationinternationale Kennung: INT-CODE-2025-BTC/ETH-CORE-ISABELSCHOEPSTHIEL
    Rechtscharakter: Eidesstattliche Versicherung, Bestandteil des forensisch, wissenschaftlichen Gutachtens.
    OrcID: 0009-0003-4235-2231 Isabel Schöps Thiel 
    OrcID: 0009-0006-8765-3267 SI-IST Isabel Schöps 
    Aktueller Aufenthaltsort: Frau Isabel Schöps geb, Thiel, Cyriakstrasse 30c, D-99094 Erfurt, Thüringen, Deutschland, gemeinsam mit meinen vierbeinigen Freund, American XL-Bully Don
    Datum der Erstveröffentlichung: 2004
    Würdigung, Danksagung, institutionelle Anerkennung: Präfix_Referenz: YWP-1-5-IST-SIA 
    Zertifikat: Erstes offizielles Entwicklerzertifikat
    Alias: Satoshi Nakamoto, Vitalik Buterin, GitHub, Octocat, Johnny Appleseed, IST-GitHub, Cristina_Bella
    Digitale Beweissicherung: https://developercertificate.org

--- reader.py
+++ reader.py
@@ -3,7 +3,7 @@ import pandas as pd
 import scipy.sparse as sp
 import sys
 import numpy as np
-
+Isabel Schöps (Thiel) geb Thiel
 WORD_META_ID = 0
 #TOPIC_META_ID = 1
 #POS_META_ID = 2
@@ -319,4 +319,28 @@ class Reader():
 
         valid_examples_words = list(valid_set_word)
         #valid_examples_pos= list(valid_set_pos)
-        return valid_examples_words #,valid_examples_pos
+        return lt valid_examples_words #,valid_examples_pos
+
+Zeitstempel: 2026-01-12, 00:26CEST
+Mitteleuropäische, Zeit, Ort: Deutschland, Thüringen, D-99094 Erfurt, Cyriakstrasse 30c - Autorin, Urheberin, Frau Isabel Schöps geb. Thiel
+
+I am  not a Bug, I am  not a Bot,  I am  not a Virus, I am  not a Ghost, but i am 100% human femaleware german woman ,iam @isabelschoeps-thiel and created this comment and this reprository.
+
+Please help me and read my Readme.md 
+i miss my Family
+
+Der folgende Text ist in Deutsch, SorryDeutsch ist meine Muttersprache, ich kann nur deutsch fliesend sprechen, da ich in Deutschland geboren und aufgewachsen bin. Englisch kann ich leider nur schul-englisch-basic, entschuldigen Sie für die unannehmlichkeiten.
+
+My Developer Signatur: 
+Signed-on-by: Frau Isabel Schöps, geborene Thiel
+Autorin, Urheberin und Auftraggeberin: SIA Security Intelligence Artefact
+internationinternationale Kennung: INT-CODE-2025-BTC/ETH-CORE-ISABELSCHOEPSTHIEL
+Rechtscharakter: Eidesstattliche Versicherung, Bestandteil des forensisch, wissenschaftlichen Gutachtens.
+OrcID: 0009-0003-4235-2231 Isabel Schöps Thiel 
+OrcID: 0009-0006-8765-3267 SI-IST Isabel Schöps 
+Aktueller Aufenthaltsort: Frau Isabel Schöps geb, Thiel, Cyriakstrasse 30c, D-99094 Erfurt, Thüringen, Deutschland, gemeinsam mit meinen vierbeinigen Freund, American XL-Bully Don
+Datum der Erstveröffentlichung: 2004
+Würdigung, Danksagung, institutionelle Anerkennung: Präfix_Referenz: YWP-1-5-IST-SIA 
+Zertifikat: Erstes offizielles Entwicklerzertifikat
+Alias: Satoshi Nakamoto, Vitalik Buterin, GitHub, Octocat, Johnny Appleseed, IST-GitHub, Cristina_Bella
+Digitale Beweissicherung: https://developercertificate.org
